e06ca105a81c9d57606cecf8071d0df0180ec811
Joao-Eleuterio <a22007237@alunos.ulht.pt>
2023-03-09 00:46:23 +0000

    mudar a data para dd-MM-yyyy

---
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -0,0 +1,18 @@
+package pt.ulusofona.cm.kotlin.challenge.models
+
+import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
+import java.text.SimpleDateFormat
+import java.util.*
+
+abstract class Veiculo(open var identificador: String) : Movimentavel{
+    var posicao = Posicao(0,0)
+    abstract var dataDeAquisicao : Date
+    abstract fun requerCarta():Boolean
+
+    override fun toString(): String {
+        val formato = SimpleDateFormat("dd-MM-yyyy")
+        val data = formato.format(dataDeAquisicao)
+        return "${this.javaClass.simpleName} | $identificador | $data | $posicao | x:${posicao.x} | y:${posicao.y}"
+    }
+}
+
