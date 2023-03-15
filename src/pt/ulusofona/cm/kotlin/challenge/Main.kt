d030abf2fa9690c540fc5ddbf43c15923d27c5e5
Joao-Eleuterio <a22007237@alunos.ulht.pt>
2023-03-15 19:29:58 +0000

    Exception andar com veiculo que requer carta e a pessoas nao tem carta

---
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -0,0 +1,23 @@
+package pt.ulusofona.cm.kotlin.challenge
+
+import pt.ulusofona.cm.kotlin.challenge.models.Carro
+import pt.ulusofona.cm.kotlin.challenge.models.Motor
+import pt.ulusofona.cm.kotlin.challenge.models.Pessoa
+import java.util.*
+
+fun main() {
+    // aqui escreves o código do programa
+
+    val carro = Carro("aabbcc", Motor(90,90))
+    val pessoa = Pessoa("aaa", Date(2002))
+    /*pessoa.comprarVeiculo(carro)
+    val pessoa1 = Pessoa("bbb", Date(2002))
+    pessoa.venderVeiculo("aabbcc",pessoa1)
+    try {
+
+    }catch (e: MenorDeIdadeException){
+       print(e.message)
+    }*/
+
+}
+    
---
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -0,0 +1,50 @@
+package pt.ulusofona.cm.kotlin.challenge.models
+
+import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoDesligadoException
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoLigadoException
+import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
+import java.util.*
+
+class Carro(override var identificador:String, var motor:Motor) : Veiculo(identificador), Ligavel {
+    override var dataDeAquisicao = Date()
+    var ligado: Boolean = false
+
+
+    override fun desligar() {
+        if (ligado) {
+            ligado = false;
+        } else {
+            throw VeiculoDesligadoException("Não pode desligar um carro desligado")
+        }
+    }
+
+    override fun estaLigado():Boolean {
+        return ligado
+    }
+
+    override fun ligar() {
+        if (!ligado) {
+            ligado = true
+        } else {
+            throw VeiculoLigadoException("Não pode ligar um carro ligado")
+        }
+    }
+    override fun requerCarta(): Boolean {
+        return true
+    }
+
+    override fun moverPara(x: Int, y: Int) {
+        if (posicao.x == x && posicao.y == y || !estaLigado()) {
+            throw AlterarPosicaoException("Não pode ir para a mesma posicao")
+        } else {
+            ligar()
+            motor.ligar()
+            posicao.alterarPosicaoPara(x, y)
+            motor.desligar()
+            desligar()
+        }
+
+    }
+
+}
