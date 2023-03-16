f171ec2d10b28e97277d0669301df48c395d8e17
Joao-Eleuterio <a22007237@alunos.ulht.pt>
2023-03-16 09:41:10 +0000

    Exception PessoaSemCarta

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -32,11 +32,10 @@ class Carro(override var identificador:String,var motor:Motor) : Veiculo(identif
     }
 
     override fun moverPara(x: Int, y: Int) {
-        if (posicao.x == x && posicao.y == y) {
+        if (posicao.x == x && posicao.y == y && !estaLigado()) {
             throw AlterarPosicaoException("${this.javaClass.simpleName} não pode ir para a mesma posicao")
         } else {
             posicao.alterarPosicaoPara(x, y)
-
         }
 
     }
